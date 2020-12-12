877cf7279c6118ed494f8420f762862d77bf73f8
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 06:10:39 +0900

    added python file

---
+++ ex230.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[68]:
+
+
+def my_print (a, b) :
+    print("왼쪽:", a)
+    print("오른쪽:", b)
+
+my_print(b=100, a=200)
+# 함수 my_print (a, b) 일때 출력한다 왼쪽은 a 오른쪽은 b 
+# my_print 그래서 왼쪽은 200 오른쪽은 100이 출력된다.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
