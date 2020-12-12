436620c65eda2eb3a7a93680fc978a5a803b994c
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 01:21:35 +0900

    added python file

---
+++ ex100.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+date = ['09/05', '09/06', '09/07', '09/08', '09/09']
+close_price = [10500, 10300, 10100, 10800, 11000]
+close_table = dict(zip(date, close_price))
+print(close_table)
+# data 와 close_price 라는 리스트를 만들고 zip 함수로 date 는 키로 close_price는 값으로 설정하고 close_table이라는 딕셔너리로 만든다 그후 출력한다
+
+
+# In[ ]:
+
+
+
+
