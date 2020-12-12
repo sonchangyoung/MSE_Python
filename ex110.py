e1ba4ae25290bb2ba42806b826ee351560121fcd
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 01:29:15 +0900

    added python file

---
+++ ex110.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+if True :
+    if False:
+        print("1")
+        print("2")
+    else:
+        print("3")
+else :
+    print("4")
+print("5")
+#if True 면 다음것을 실행한다 거짓이면 1,2를 출력하고 거짓이 아님( 참)이면 3을 출력한다 첫줄과 연관되는 
+#else 는 4를출력해라 참이 아니므로 4를 출력하지않는다 마지막 5를 출력하라는 명령은 위의 식과 연관되지않은 식이고 출력된다.
+
+
+# In[ ]:
+
+
+
+
