0b730a56fdc330298ec6dfaed99690590fdb2b21
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 04:00:15 +0900

    added pythom file

---
+++ ex170.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[40]:
+
+
+result = 1
+for i in range(1, 11) :
+    result = result*i
+print(result)
+#첫 숫자 1에서 1부터 10까지의 숫자를 조건하에 반복한다 결과는 결과 곱하기 변수 i 
+#곱해지는 변수가 10이 될 때까지 반복한다 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
