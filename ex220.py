b6fbe24aa5873ea7c9c45416146d22e73812d037
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 05:58:04 +0900

    added python file

---
+++ ex220.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[67]:
+
+
+def print_max(a, b, c) :
+    if a > b and a > c: #a가 b 랑 c 보다 크면  a를 출력하고
+        print(a)
+    elif b > c and b > a: #b가 c랑 a 보다 크면 b를 출력하고
+        print(b)
+    else:
+        print(c) #나머진 c를 출력하여라.
+print_max(1,3,5)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
