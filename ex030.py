17532fb2f1bd586dd7b196ccdf192a7f34fe7d3d
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 00:15:39 +0900

    added python file

---
+++ ex030.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+string = 'abcd'
+string.replace('b', 'B')
+print(string)
+#abcd가 그대로 출력이 된다 replace 는 변경될 새로운 문자를 바꿔준다 대문자 소문자는 바뀌지 않는다
+
+
+# In[ ]:
+
+
+
+
