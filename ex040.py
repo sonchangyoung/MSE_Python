f4631d2fd92e5d2692376d4fc06627e6e04549e3
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 00:20:43 +0900

    added python file

---
+++ ex040.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+data = "   삼성전자    "
+data1 = data.strip()
+print(data1)
+#strip를 사용하면 좌우의 공백이 사라진다 strip을 사용하면 공백을 없에고 출력 할 수 있다.
+
+
+# In[ ]:
+
+
+
+
