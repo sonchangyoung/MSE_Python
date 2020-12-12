3c016d690ac8cd1aa430c6d2b7e541be1bb02bb8
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 06:18:08 +0900

    added python file

---
+++ ex250.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[70]:
+
+
+import numpy
+for i in numpy.arange(0, 5, 0.1):
+    print(i)
+    # numpy 모듈을 가져오고 0에서 5까지 0.1 씩 증가하는 for 문을 만든다
+    #5.0이 될때까지 반복된다.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
