d52baad2f56b87fc44e19b3dd05904c6638b9cb0
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 06:36:23 +0900

    added python file

---
+++ ex260.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[72]:
+
+
+class OMG : 
+    def print() :  #print(self) 가 있어야함 
+        print("Oh my god")
+
+>>> >>> myStock = OMG()
+>>> myStock.print()
+ #class 로 객체를 만들고 . 찍고 매소드를 호출하면 자동으로 객체가 첫번쨰로 가기떄문에 self를 넣어줘야함
+# print() 의 arguments 가 공백인 상태여서 에러가 발생한것임 어디서 오는지 기입을 해줘야함 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
