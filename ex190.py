6f23bbb32fee3f7359de69516a66eb1ae0a69c39
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 04:30:33 +0900

    added python file

---
+++ ex190.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[49]:
+
+
+apart = [ [101, 102], [201, 202], [301, 302] ]
+for row in apart:
+    for col in row:
+        print(col, "호")
+print("-" * 5)
+#아파트의 층수를 분류하고 for 함수를 이용해서 숫자가 작은 순서대로 출력하며
+#전부 출력하면 마지막으로  "-"를 *를 이용해서 5개 출력한다.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
