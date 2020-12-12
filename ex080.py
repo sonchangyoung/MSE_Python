cde222314c6645f4cc083730d24e54277ff77b05
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 00:37:38 +0900

    added python file

---
+++ ex080.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+data = tuple(range(2, 100, 2))
+print( data )
+#tuple함수로 2에서99 까지에서 2씩 증가하는 수를 출력한다 2부터 시작하여 2씩 증가하면 전부 짝수이다.
+
+
+# In[ ]:
+
+
+
+
