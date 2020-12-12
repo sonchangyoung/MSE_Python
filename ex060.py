06347d63789b384d90d282fef76ade3b9b949f62
sonchangyoung <75737922+sonchangyoung@users.noreply.github.com>
2020-12-13 00:29:23 +0900

    add python file

---
+++ ex060.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+nums = [1, 2, 3, 4, 5]
+average = sum(nums) / len(nums)
+print(average)
+#sum은 합을 구해주고 len은 원소가 몇 개 인지 구해준다 합 나누기 원소의 수는 즉 평균이다.
+
+
+# In[ ]:
+
+
+
+
